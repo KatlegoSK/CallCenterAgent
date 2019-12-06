1d9e10e61348e38f38a540c8e3183954840891e8
Katlego-The-Kat <sendmessageresponse@gmail.com>
2019-12-06 12:25:27 +0200

    redefined the manager declaration

--- src/main/java/com/xib/assessment/ManagerController.java
+++ src/main/java/com/xib/assessment/ManagerController.java
@@ -3,9 +3,7 @@ package com.xib.assessment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
@@ -14,6 +12,9 @@ import javax.validation.Valid;
 
 @RestController
 public class ManagerController {
+	
+	@Autowired
+	private ManagerRepository managerRepository;
 
 	/* Create Manager */
 	@PostMapping("/manager")
